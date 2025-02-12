f4f600fbbf9d7a1294c6ea8b2b721990f3926b51
Pavel Prydokhin <57828585+pavelpryadokhin@users.noreply.github.com>
2025-02-13 00:07:07 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,6 @@ $(function() {
     if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
     if (pos2 > $('#about').offset().top)      { highlightLink('about'); }
     if (pos2 > $('#portfolio').offset().top)  { highlightLink('portfolio'); }
-    if (pos2 > $('#blog').offset().top)       { highlightLink('blog'); }
     if (pos2 > $('#contact').offset().top || 
         pos + $(window).height() === $(document).height()) { 
           highlightLink('contact');
